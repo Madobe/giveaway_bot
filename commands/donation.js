ca122b41061206fc8ea0543aa75ba4a15145f042
Madobe <sin_emerald@hotmail.com>
2018-10-30 19:40:52 -0600

    Make it case-insensitive

--- commands/donation.js
+++ commands/donation.js
@@ -20,7 +20,7 @@ If you would not like to add a restriction, just type \`none\`.`,
   let userResponses = [];
   for(let i = 0; i < questions.length; i++) {
     const response = await userInput.getSingleInput(message, questions[i]);
-    if(response === "cancel") return;
+    if(response.toLowerCase() === "cancel") return;
     userResponses.push(response);
   }
 
