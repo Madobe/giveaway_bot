81b23c5b9a1e726796d18b357797e7c30c42b4ab
Madobe <sin_emerald@hotmail.com>
2018-11-03 13:23:29 -0600

    Test role command

--- commands/role.js
+++ commands/role.js
@@ -1,8 +1,10 @@
 exports.run = async (client, message, args) => {
   if(args.length < 1) return message.channel.send("No role name to look up specified.");
   const roleName = args.join(" ");
-  const roleID = message.guild.roles.find("name", roleName).id;
-  message.channel.send(roleID);
+  const role = message.guild.roles.find("name", roleName);
+  if(!role) return null;
+  const roleID = role.id;
+  return message.channel.send(roleID);
 };
 
 exports.conf = {
