bfe9d914d1b141e90f8d55292d7a0df4edef4c84
Madobe <sin_emerald@hotmail.com>
2018-11-03 11:39:31 -0600

    Rename command

--- commands/donationresponses.js
+++ commands/donationresponses.js
@@ -3,7 +3,7 @@ const storage = require("node-persist");
 exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
   const channelId = message.content.split(" "). slice(1)[0];
   message.channel.send(`Donations notification channel has been set to <#${channelId}> with ID ${channelId}.`);
-  await storage.init();
+  await storage.init(args.storageOpts);
   await storage.setItem("donationNotificationChannelId", channelId);
 };
 
