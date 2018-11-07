3277ab95ca377572befd41128d12b4eecf51fcc1
Madobe <sin_emerald@hotmail.com>
2018-11-07 10:35:33 -0700

    Mock up API calls for tests

--- test/commands/top_test.js
+++ test/commands/top_test.js
@@ -2,12 +2,34 @@
 const chai = require("chai");
 chai.use(require("chai-string"));
 const expect = chai.expect;
+const nock = require("nock");
 const command = require("../../commands/top");
 
 describe("top", () => {
   const message = { channel: { send: (text) => { return text; } } };
 
   describe("#run", () => {
+    nock.disableNetConnect();
+    nock(/www\.googleapis\.com/)
+      .get(/.*/)
+      .reply(200, {});
+    nock(/www\.googleapis\.com/)
+      .persist()
+      .filteringPath(() => "/")
+      .post("/")
+      .reply(200, {});
+    nock(/sheets\.googleapis\.com/)
+      .persist()
+      .get(/.*/)
+      .reply(200, {
+        values: [
+          ["One", 1],
+          ["Two", 2],
+          ["Three", 3],
+          ["Four", 4]
+        ]
+      });
+
     it("returns a string", async () => {
       const result = await command.run({}, message, {});
 
--- test/modules/leaderboard_test.js
+++ test/modules/leaderboard_test.js
@@ -2,9 +2,31 @@
 const chai = require("chai");
 chai.use(require("chai-string"));
 const expect = chai.expect;
+const nock = require("nock");
 const leaderboard = require("../../modules/leaderboard");
 
 describe("leaderboard", () => {
+  nock.disableNetConnect();
+  nock(/www\.googleapis\.com/)
+    .get(/.*/)
+    .reply(200, {});
+  nock(/www\.googleapis\.com/)
+    .persist()
+    .filteringPath(() => "/")
+    .post("/")
+    .reply(200, {});
+  nock(/sheets\.googleapis\.com/)
+    .persist()
+    .get(/.*/)
+    .reply(200, {
+      values: [
+        ["One", 1],
+        ["Two", 2],
+        ["Three", 3],
+        ["Four", 4]
+      ]
+    });
+
   describe("#generate", () => {
     it("returns a string", async () => {
       const result = await leaderboard.generate();
