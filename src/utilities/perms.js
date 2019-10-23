c4ed5aeab62af4a83fbda62504d644753ac23d5c
Madobe <sin_emerald@hotmail.com>
2019-10-23 03:36:02 -0600

    Fix perm checking in DMs

--- src/utilities/perms.js
+++ src/utilities/perms.js
@@ -3,39 +3,41 @@ const PERMS = {
   MODERATOR: 4,
   GIVEAWAY: 2,
   NONE: 1
-};
+}
 
 const calculatePerms = (message) => {
-  let perms = 1;
+  let perms = 1
 
   if (message.author.id === "178472840956215296") {
-    perms = perms | PERMS.CREATOR | (PERMS.CREATOR - 1);
+    perms = perms | PERMS.CREATOR | (PERMS.CREATOR - 1)
   }
 
+  if (message.member === null) return perms
+
   if (message.member.roles.get(process.env.MODERATOR_ROLE) !== undefined) {
-    perms = perms | PERMS.MODERATOR;
+    perms = perms | PERMS.MODERATOR
   }
 
   if (message.member.roles.get(process.env.GIVEAWAY_ROLE) !== undefined) {
-    perms = perms | PERMS.GIVEAWAY;
+    perms = perms | PERMS.GIVEAWAY
   }
 
-  return perms;
-};
+  return perms
+}
 
 const checkPerms = (target, message) => {
   try {
-    const targetPerms = PERMS[target.toUpperCase()];
-    const perms = calculatePerms(message);
+    const targetPerms = PERMS[target.toUpperCase()]
+    const perms = calculatePerms(message)
 
-    return (targetPerms & perms) === targetPerms;
+    return (targetPerms & perms) === targetPerms
   } catch (e) {
-    console.error(e);
-    return false;
+    console.error(e)
+    return false
   }
-};
+}
 
 module.exports = {
   calculatePerms,
   checkPerms
-};
+}
