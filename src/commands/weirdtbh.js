86e44569cbc0b7f9a93e64e6f78a0c63fd3bd0cd
Madobe <sin_emerald@hotmail.com>
2020-03-23 02:21:50 -0600

    Was missing command internals

---
+++ src/commands/weirdtbh.js
@@ -0,0 +1,11 @@
+/**
+ * AlphA00X's custom command.
+ */
+exports.run = async (client, message, args) => {
+  return message.channel.send("https://evidence.hep.gg/dQWa9xOI")
+}
+
+
+exports.conf = {
+  permissionLevel: "none"
+}
