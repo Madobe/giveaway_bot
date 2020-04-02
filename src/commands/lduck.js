30088583afb83ae12bd498342adc2ce1d765547f
LordOfErebus <lordoferebus43@gmail.com>
2020-04-02 20:59:01 +1300

    Update and rename duck.js to lduck.js

--- src/commands/lduck.js
+++ src/commands/lduck.js
@@ -1,5 +1,5 @@
 /**
- * Blam Blam's custom command.
+ * Rose's custom command.
  */
 exports.run = async (client, message, args) => {
   return message.channel.send("https://i.imgur.com/Op8zvp9.png")
